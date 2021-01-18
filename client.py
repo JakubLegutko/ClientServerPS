90d2ab78a8977975b4a6fa82b4ae2dc4eee7aa40
jlegutko@student.agh.edu.pl <jlegutko@student.agh.edu.pl>
2021-01-19 00:30:28 +0100

    now client reads from COM6 port and streams data to server. Need to work on stopping condition.

--- client.py
+++ client.py
@@ -1,6 +1,9 @@
 import socket  
+import serial
 
 
+
+ser = serial.Serial('COM6',baudrate = 115200, timeout = 1)
 HEADER = 64
 PORT = 5050
 FORMAT = 'utf-8'
@@ -9,6 +12,20 @@ SERVER = "192.168.56.1"
 ADDR = (SERVER, PORT)
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(ADDR)
+def read_sensor_data_and_send():
+    for i in range(100) :
+         KLO5Data = ser.readline()
+         print(KLO5Data)
+         msg_length = len(KLO5Data)
+         send_length = str(msg_length).encode(FORMAT)
+         send_length += b' ' *(HEADER - len(send_length))
+         client.send(send_length)
+         client.send(KLO5Data)
+         print(client.recv(2048).decode(FORMAT))
+
+   
+
+
 
 def send(msg):
     message = msg.encode(FORMAT)
@@ -25,4 +42,5 @@ input()
 send("Hello World!")
 input()
 send("Hello World!")
+read_sensor_data_and_send()
 send(DISCONNECT_MSG)
